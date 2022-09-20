00b49d81f9c3e352947ae27766b1cb0bc605d68e
pawan-kashyap01 <pawan.cse2021@gmail.com>
2022-09-20 12:49:03 +0530

    Pre-final Commit

--- app.js
+++ app.js
@@ -1,41 +1,48 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const app = express();
+const cors = require('cors');
+const articles = require('./routes/article');
+require('dotenv').config();
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
+// Enable CORS
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,PATCH,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+  // intercept OPTIONS method
+  if (req.method === 'OPTIONS') {
+    res.send(200);
+  } else {
+    next();
+  }
+});
+
+app.use('/api/articles',articles);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
+  console.log('Not Found');
   next(createError(404));
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.json({
+    message: err.message,
+    error : err
+    // error: req.app.get('env') === 'development' ? err : {}
+  });
 });
 
 module.exports = app;
